a928c83a62e3dfbc1daa5b341c59cff72b6a0f09
wangzhaojun <wangzhaojun@xiaomi.com>
2021-01-26 01:42:28 +0800

    first commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    title: '翔工作室技术博客',
+    description: 'Flying Studio',
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+        dateFormat: 'YYYY-MM-DD',
+        nav: [
+            { text: '文章', link: '/' },
+            { text: '标签', link: '/tag/' },
+            { text: '关于', link: '/about' },
+            { text: '成员', link: '/member' }
+        ],
+        footer: {
+            concact: [
+                { type: 'github', link: 'https://github.com/Suvern' },
+                { type: 'mail', link: 'flyingstudio@yeah.net' },
+                { type: 'web', link: 'https://atcumt.com' },
+            ],
+            copyright: [
+                { text: 'MIT Licensed | Copyright © 2021 翔工作室' }
+            ]
+        },
+        directories: [{
+            id: '文章',
+            dirname: '_posts',
+            path: '/'
+        }],
+        frontmatters: [{
+            id: '标签',
+            keys: ['tag', 'tags'],
+            path: '/tag/',
+        }],
+    },
+    dest: './dist',
+    head: [
+        ['link', { rel: 'icon', href: '/assets/studio.png' }]
+    ]
+}
---
+++ blog/member.md
@@ -0,0 +1,8 @@
+# 成员
+
+| 姓名   | 博客地址                                     | 介绍         |
+| ------ | -------------------------------------------- | ------------ |
+| 张肖飞 | [八一](https://www.cnblogs.com/bay1/)        | 计算机 16 级 |
+| 王昭君 | [王昭君的个人博客](https://wangzhaojun.wiki) | 信控 17 级   |
+| 李金澎 | [DwyaneLee](https://lijinpengt.github.io/)   | 信控 17 级   |
+| 任俊达 | [Lsmg 的大学之路](https://blog.lsmg.xyz/)    | 计算机 18 级 |
